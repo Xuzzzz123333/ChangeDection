bf427512c58aa98d2a853cc460a4f5bce7dfc0c2
ChangeDINO Dynamic Policy Fix <changedino-fix@local>
2026-05-15 09:01:25 +0000

    fix: remove policy_entropy_weight from DINOV3Wrapper kwargs (only used in Model.forward)

--- model/ChangeDINO.py
+++ model/ChangeDINO.py
@@ -236,7 +236,6 @@ class Encoder(nn.Module):
                 "policy_budget_granularity", "per_layer"
             ),
             policy_min_keep=kwargs.get("policy_min_keep", 0.0),
-            policy_entropy_weight=kwargs.get("policy_entropy_weight", 0.1),
             policy_mlp_gate=kwargs.get("policy_mlp_gate", False),
             policy_hidden_dim=kwargs.get("policy_hidden_dim", 256),
             num_prefix_tokens=kwargs.get("num_prefix_tokens", -1),
